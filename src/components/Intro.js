df3df2af735395d721ce1e1cbcc98527dae8f2f7
JChenster <jeffreyzhenzhichen@gmail.com>
2021-06-07 18:14:46 -0400

    Added intro and socials

---
+++ src/components/Intro.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Intro.css";
+import pfp from "../images/pfp.jpg"
+
+const Intro = () => {
+    const about = `Hi there! Welcome to my website! 
+                    I'm Jeffrey, a rising sophomore at Yale College from Brooklyn, NYC. 
+                    I'm studying Computer Science and Economics. 
+                    Some of my interests include software engineering, AI/ML, data science, investing, personal finance, public policy, and international relations. 
+                    I enjoy long walks and scooter rides while listening to music and podcasts, as well as playing soccer.`
+    
+    return (
+        <div class="container">
+            <div class = "left">
+                <img src={pfp} alt = "Me" id="pfp"/>
+            </div>
+            <div class = "right"> {about} </div>
+        </div>
+    );
+};
+
+export default Intro;
